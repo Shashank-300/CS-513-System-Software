9354ae9c4f455b822cd140367fa7b1c5decc0985
Shashank Vyas <vyasshashank300@gmail.com>
2024-10-20 22:48:11 +0530

    Delete Mini_Project directory

--- Mini_Project/feedback.h
+++
@@ -1,4 +0,0 @@
-struct Feedback {
-    int userID;          // Customer ID who gave the feedback
-    char feedbackText[500]; // The feedback message
-};
--- Mini_Project/loan.h
+++
@@ -1,8 +0,0 @@
-struct Loan {
-    int customerID;       // ID of the customer requesting the loan
-    int loanID;           // Unique loan ID
-    int assignedEmployee; // ID of the employee assigned to handle the loan (0 if unassigned)
-    double amount;        // Loan amount
-    int accountID;        // Account ID associated with the loan
-    int isAccepted; 
-};
